d794bb750bdd5f4e224c432f4b169716ddf523c8
Andi Hellmund <am.hellmund@gmail.com>
2024-08-28 22:06:28 +0200

    next version of scanner

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,17 @@
-use lox::add;
+use std::path::PathBuf;
 
-fn main () {
-    let a = add(10, 20);
-    println!("{a}");
+use clap::Parser;
+use lox::execute;
+use anyhow::Result;
+
+#[derive(Parser)]
+struct Args {
+    file_path: PathBuf,
+}
+
+fn main () -> Result<()>  {
+    let args = Args::parse();
+    println!("File argument is {}", args.file_path.display());
+    execute(&args.file_path)?;
+    Ok(())
 }
