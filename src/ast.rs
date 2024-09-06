acf6f5406f71b833fa65d36e4754828454abee62
Andi Hellmund <am.hellmund@gmail.com>
2024-09-06 22:06:27 +0200

    add ast

---
+++ src/ast.rs
@@ -0,0 +1,37 @@
+pub enum UnaryOperator {
+    Minus,
+    Not,
+}
+
+pub enum BinaryOperator {
+    Add,
+    Substract,
+    Multiply,
+    Divide,
+    LessThan,
+    LessThanOrEqual,
+    GreaterThan,
+    GreaterThanOrEqual,
+    Equal,
+    NotEqual,
+}
+
+pub enum Literal {
+    Number(f64),
+    String(String),
+    Boolean(bool),
+    Nil,
+}
+
+pub enum Expr {
+    Binary {
+        lhs: Box<Expr>,
+        op: BinaryOperator,
+        rhs: Box<Expr>,
+    },
+    Unary {
+        op: UnaryOperator,
+        expr: Box<Expr>,
+    },
+    Literal(Literal),
+}
--- src/lib.rs
+++ src/lib.rs
@@ -2,9 +2,9 @@ use std::path::Path;
 use std::fs;
 use anyhow::{Context, Result};
 
-mod scanner;
+mod ast;
 mod diagnostics;
-
+mod scanner
 
 pub fn execute(file_path: &Path) -> Result<()> {
     let content = fs::read_to_string(file_path)
