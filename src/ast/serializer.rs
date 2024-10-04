cbd903de0697973b99c3c763a7aed7bbdcf985bc
Andi Hellmund <am.hellmund@gmail.com>
2024-10-04 22:23:21 +0200

    intermediate state

--- src/ast/serializer.rs
+++ src/ast/serializer.rs
@@ -12,7 +12,7 @@ use crate::{
     diagnostics::LocationSpan,
 };
 
-use super::{Stmt, StmtData};
+use super::{ExprData, Stmt, StmtData};
 
 #[derive(Clone)]
 pub struct AstSerializerOptions {
@@ -37,245 +37,169 @@ pub struct AstSerializerOptions {
 /// This class may be used for different purposes, e.g. to store the AST to a file or use it for
 /// testing purpose to easily compare two ASTs structurally.
 ///
+/// Note on Performance: the performance of the serialization is sub-optimal due to many inefficient
+/// string operations and recursions. For example: the indentation is achieved by "unwinding" the
+/// call stack und repeatedly adding the next-level indentation. The design however has nevertheless
+/// been chosen due to simplicity of the serialization code itself. The sub-optimal performance is accepted
+/// as of now because the serialization is seldomly used in production and mostly for development, debugging
+/// and testing purpose.
 struct AstTopologicalSerializer {
     options: AstSerializerOptions,
-    indent: i64,
-    content: String,
 }
 
 impl AstTopologicalSerializer {
     const INDENT_NEXT_LEVEL: i64 = 2;
 
-    fn new(options: AstSerializerOptions, indent: i64) -> Self {
-        Self {
-            options,
-            indent,
-            content: String::default(),
+    pub fn new(options: AstSerializerOptions) -> Self {
+        Self { options }
+    }
+
+    pub fn serialize(self, stmt: &Stmt) -> String {
+        self.serialize_stmt(stmt)
+    }
+
+    fn generate_ast_entry(
+        &self,
+        tag: &str,
+        loc: &LocationSpan,
+        sub_entries: Vec<String>,
+    ) -> String {
+        let mut content = String::default();
+        if self.options.include_location {
+            content += &format!("({} [{}]\n", tag, loc.to_string());
+        } else {
+            content += &format!("({}\n", tag)
         }
+        for sub_entry in sub_entries {
+            content += &format!(
+                "{}{}",
+                Self::get_indent_as_string(Self::INDENT_NEXT_LEVEL),
+                sub_entry,
+            );
+        }
+        content += ")\n";
+        content
     }
 
-    fn new_nested_serializer(&self) -> Self {
-        Self::new(self.options.clone(), self.indent + Self::INDENT_NEXT_LEVEL)
-    }
-
-    fn serialize(mut self, stmt: &Stmt) -> String {
-        self.serialize_stmt(stmt);
-        self.content
-    }
-
-    fn serialize_stmt(&mut self, stmt: &Stmt) {
+    fn serialize_stmt(&self, stmt: &Stmt) -> String {
+        let loc = stmt.get_loc();
         match &stmt.data {
-            StmtData::Block { statements } => {}
-            StmtData::Expr { expr } => {}
+            StmtData::Block { statements } => self.serialize_stmt_list("block", loc, statements),
+            StmtData::Expr { expr } => {
+                self.generate_ast_entry("expr", loc, vec![self.serialize_expr(expr)])
+            }
             StmtData::If {
                 condition,
                 if_statement,
                 else_statement,
-            } => {}
-            StmtData::List { statements } => {}
-            StmtData::Print { expr } => {}
+            } => {
+                let mut sub_entries = vec![
+                    self.serialize_expr(condition),
+                    self.serialize_stmt(if_statement),
+                ];
+                if let Some(else_statement) = else_statement {
+                    sub_entries.push(self.serialize_stmt(else_statement));
+                }
+                self.generate_ast_entry(tag, loc, sub_entries)
+            }
+            StmtData::List { statements } => self.serialize_stmt_list("list", loc, statements),
+            StmtData::Print { expr } => {
+                self.generate_ast_entry_single("print", loc, self.serialize_expr(expr))
+            }
             StmtData::VarDecl {
                 identifier,
                 init_expr,
-            } => {}
-            StmtData::While { condition, body } => {}
+            } => self.generate_ast_entry(
+                "var-decl",
+                loc,
+                vec![identifier.clone(), self.serialize_expr(init_expr)],
+            ),
+            StmtData::While { condition, body } => self.generate_ast_entry_single(
+                "while",
+                loc,
+                vec![self.serialize_expr(condition), self.serialize_stmt(body)],
+            ),
         }
     }
-}
 
-pub fn serialize_ast(stmt: &Stmt, options: AstSerializerOptions) -> String {
-    let serializer = AstTopologicalSerializer::new(options);
-    serializer.serialize(stmt)
-}
-
-/// Prints the AST to the command-line
-pub fn print_ast(stmt: &Stmt) -> String {
-    print_stmt(stmt, 0)
-}
-
-fn get_indent_as_string(indent: i64) -> String {
-    std::iter::repeat(" ")
-        .take(indent as usize)
-        .collect::<String>()
-}
-
-fn stringify_statements(
-    statements: &Vec<Stmt>,
-    ast_tag: &str,
-    loc: &LocationSpan,
-    indent: i64,
-) -> String {
-    let mut statement_string = format!(
-        "{}[{}] [{}]\n",
-        get_indent_as_string(indent),
-        ast_tag,
-        loc.to_string()
-    );
-    for statement in statements {
-        statement_string += &print_stmt(statement, indent + INDENT_NEXT_LEVEL);
-    }
-    statement_string
-}
-
-fn print_stmt(stmt: &Stmt, indent: i64) -> String {
-    match stmt {
-        Stmt::List { statements, loc } => stringify_statements(statements, "List", loc, indent),
-        Stmt::Block { statements, loc } => stringify_statements(statements, "Block", loc, indent),
-        Stmt::VarDecl {
-            identifier,
-            init_expr,
-            loc,
-        } => {
-            format!(
-                "{}[VarDecl] [{}]\n{}\"{}\n{}\n",
-                get_indent_as_string(indent),
-                loc.to_string(),
-                get_indent_as_string(indent + INDENT_NEXT_LEVEL),
-                identifier,
-                print_expr(init_expr, indent + INDENT_NEXT_LEVEL),
-            )
-        }
-        Stmt::Expr { expr, loc } => {
-            format!(
-                "{}[Expr] [{}]\n{}\n",
-                get_indent_as_string(indent),
-                loc.to_string(),
-                print_expr(expr, indent + INDENT_NEXT_LEVEL),
-            )
-        }
-        Stmt::Print { expr, loc } => {
-            format!(
-                "{}[Print] [{}]\n{}\n",
-                get_indent_as_string(indent),
-                loc.to_string(),
-                print_expr(expr, indent + INDENT_NEXT_LEVEL),
-            )
-        }
-        Stmt::If {
-            condition,
-            if_statement,
-            else_statement,
-            loc,
-        } => {
-            if else_statement.is_some() {
+    fn serialize_expr(&self, expr: &Expr) -> String {
+        let loc = expr.get_loc();
+        match &expr.data {
+            ExprData::Binary { lhs, op, rhs } => self.generate_ast_entry(
+                &op.to_string(),
+                loc,
+                vec![self.serialize_expr(lhs), self.serialize_expr(rhs)],
+            ),
+            ExprData::Unary { op, expr } => {
+                let op_str = op.to_string();
+                let expr_str = print_expr(expr, indent + INDENT_NEXT_LEVEL);
                 format!(
-                    "{}[If] [{}]\n{}\n{}\n{}",
+                    "{}{} [{}]\n{}",
                     get_indent_as_string(indent),
+                    op_str,
                     loc.to_string(),
-                    print_expr(condition, indent + INDENT_NEXT_LEVEL),
-                    print_stmt(if_statement, indent + INDENT_NEXT_LEVEL),
-                    print_stmt(else_statement.as_ref().unwrap(), indent + INDENT_NEXT_LEVEL),
-                )
-            } else {
-                format!(
-                    "{}[If] [{}]\n{}\n{}",
-                    get_indent_as_string(indent),
-                    loc.to_string(),
-                    print_expr(condition, indent + INDENT_NEXT_LEVEL),
-                    print_stmt(if_statement, indent + INDENT_NEXT_LEVEL),
+                    expr_str
                 )
             }
-        }
-        Stmt::While {
-            condition,
-            body,
-            loc,
-        } => {
-            format!(
-                "{}[While] [{}]\n{}\n{}",
+            Expr::Grouping { expr, loc } => {
+                let expr_str = print_expr(expr, indent + INDENT_NEXT_LEVEL);
+                format!(
+                    "{}() [{}]\n{}",
+                    get_indent_as_string(indent),
+                    loc.to_string(),
+                    expr_str
+                )
+            }
+            Expr::Variable { name, loc } => {
+                format!(
+                    "{}<var: {}> [{}]",
+                    get_indent_as_string(indent),
+                    name,
+                    loc.to_string()
+                )
+            }
+            Expr::Assign { name, expr, loc } => {
+                let expr_str = print_expr(expr, indent + INDENT_NEXT_LEVEL);
+                format!(
+                    "{}= [{}]\n{}{}\n{}",
+                    get_indent_as_string(indent),
+                    loc.to_string(),
+                    get_indent_as_string(indent + INDENT_NEXT_LEVEL),
+                    name,
+                    expr_str,
+                )
+            }
+            Expr::Literal {
+                literal: Literal::Boolean(value),
+                loc,
+            } => format!(
+                "{}{} [{}]",
                 get_indent_as_string(indent),
-                loc.to_string(),
-                print_expr(&condition, indent + INDENT_NEXT_LEVEL),
-                print_stmt(&body, indent + INDENT_NEXT_LEVEL),
-            )
-        }
-    }
-}
-
-fn print_expr(expr: &Expr, indent: i64) -> String {
-    match expr {
-        Expr::Binary { lhs, op, rhs, loc } => {
-            let lhs_str = print_expr(lhs, indent + INDENT_NEXT_LEVEL);
-            let op_str = op.to_string();
-            let rhs_string = print_expr(rhs, indent + INDENT_NEXT_LEVEL);
-            format!(
-                "{}{} [{}]\n{}\n{}",
-                get_indent_as_string(indent),
-                op_str,
-                loc.to_string(),
-                lhs_str,
-                rhs_string
-            )
-        }
-        Expr::Unary { op, expr, loc } => {
-            let op_str = op.to_string();
-            let expr_str = print_expr(expr, indent + INDENT_NEXT_LEVEL);
-            format!(
-                "{}{} [{}]\n{}",
-                get_indent_as_string(indent),
-                op_str,
-                loc.to_string(),
-                expr_str
-            )
-        }
-        Expr::Grouping { expr, loc } => {
-            let expr_str = print_expr(expr, indent + INDENT_NEXT_LEVEL);
-            format!(
-                "{}() [{}]\n{}",
-                get_indent_as_string(indent),
-                loc.to_string(),
-                expr_str
-            )
-        }
-        Expr::Variable { name, loc } => {
-            format!(
-                "{}<var: {}> [{}]",
-                get_indent_as_string(indent),
-                name,
+                value,
                 loc.to_string()
-            )
-        }
-        Expr::Assign { name, expr, loc } => {
-            let expr_str = print_expr(expr, indent + INDENT_NEXT_LEVEL);
-            format!(
-                "{}= [{}]\n{}{}\n{}",
+            ),
+            Expr::Literal {
+                literal: Literal::Nil,
+                loc,
+            } => format!("{}nil [{}]", get_indent_as_string(indent), loc.to_string()),
+            Expr::Literal {
+                literal: Literal::String(value),
+                loc,
+            } => format!(
+                "{}\"{}\" [{}]",
                 get_indent_as_string(indent),
-                loc.to_string(),
-                get_indent_as_string(indent + INDENT_NEXT_LEVEL),
-                name,
-                expr_str,
-            )
+                value,
+                loc.to_string()
+            ),
+            Expr::Literal {
+                literal: Literal::Number(value),
+                loc,
+            } => format!(
+                "{}{} [{}]",
+                get_indent_as_string(indent),
+                value,
+                loc.to_string()
+            ),
         }
-        Expr::Literal {
-            literal: Literal::Boolean(value),
-            loc,
-        } => format!(
-            "{}{} [{}]",
-            get_indent_as_string(indent),
-            value,
-            loc.to_string()
-        ),
-        Expr::Literal {
-            literal: Literal::Nil,
-            loc,
-        } => format!("{}nil [{}]", get_indent_as_string(indent), loc.to_string()),
-        Expr::Literal {
-            literal: Literal::String(value),
-            loc,
-        } => format!(
-            "{}\"{}\" [{}]",
-            get_indent_as_string(indent),
-            value,
-            loc.to_string()
-        ),
-        Expr::Literal {
-            literal: Literal::Number(value),
-            loc,
-        } => format!(
-            "{}{} [{}]",
-            get_indent_as_string(indent),
-            value,
-            loc.to_string()
-        ),
     }
 }
